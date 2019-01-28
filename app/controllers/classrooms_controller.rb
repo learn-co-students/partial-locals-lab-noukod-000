df93de54957f64b5b79d7404edb1da2a305b16e0
Gilbert Torchon <gilbert@noukod.com>
2019-01-28 19:45:07 +0000

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,15 @@
 class ClassroomsController < ApplicationController
   def show
+
     @classroom = Classroom.find(params[:id])
     @student = @classroom.oldest_student
+    puts @student.inspect
   end
 
   def index
+    Classroom.all.each_with_index do |classroom, i|
+      classroom.students << [Student.limit(8).offset(i*2)]
+    end
     @classrooms = Classroom.all
   end
 end
