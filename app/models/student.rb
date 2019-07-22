8b0eea9893655a176a79a8c13dc19bcccd3ebd52
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-07-22 11:25:19 -0400

    Second Commit

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,6 +9,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
